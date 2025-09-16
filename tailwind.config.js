ead9b165003c1f3169d86596b18e109fcc0d5b76
comptanetquebec <comptanetquebec@gmail.com>
2025-09-15 20:20:20 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,22 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cq: {
+          primary: "#0f3b74",   // bleu ComptaNet
+          primaryDark: "#0c2f5c",
+        },
+      },
+      borderRadius: {
+        xl2: "1rem",
+      },
+    },
+  },
+  plugins: [],
+};
