bcc08ab693483821145d4e12dfbedf2a77231964
comptanetquebec <comptanetquebec@gmail.com>
2025-10-23 18:19:34 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
-// Configuration officielle PostCSS pour Next.js + TailwindCSS
-// Ce fichier est lu automatiquement par Next au moment du build.
-
 module.exports = {
   plugins: {
-    tailwindcss: {},     // Plugin Tailwind
-    autoprefixer: {},    // Ajoute automatiquement les préfixes CSS
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
