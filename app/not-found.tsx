6b7db8eccfcfa8358394a3d1f10d82fb03887306
comptanetquebec <comptanetquebec@gmail.com>
2025-10-23 19:38:04 -0400

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ export default function NotFound() {
     <main className="min-h-screen flex flex-col items-center justify-center text-center p-6">
       <h1 className="text-3xl font-bold mb-4 text-blue-800">Page non trouvée</h1>
       <p className="text-gray-600 mb-6">
-        Désolé, la page que vous cherchez n’existe pas ou a été déplacée.
+        Désolé, la page demandée n’existe pas ou a été déplacée.
       </p>
       <a
         href="/"
