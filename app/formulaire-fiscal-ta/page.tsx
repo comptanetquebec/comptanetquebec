7ce5f9490dc62c1698d501e79965ebaeb6c1a659
comptanetquebec <comptanetquebec@gmail.com>
2026-02-03 12:11:01 -0500

    Update page.tsx

--- app/formulaire-fiscal-ta/page.tsx
+++ app/formulaire-fiscal-ta/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import "../formulaire-fiscal/formulaire-fiscal.css";
+import "./formulaire-fiscal.css";
 
 import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
