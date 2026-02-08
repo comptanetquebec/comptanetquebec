a9a2c389e7c0c6eeafe84b0db75dbc88c885a09f
comptanetquebec <comptanetquebec@gmail.com>
2026-02-08 12:15:21 -0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
+// lib/supabaseClient.ts
+import { createBrowserClient } from "@supabase/ssr";
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-if (!url) throw new Error('NEXT_PUBLIC_SUPABASE_URL is not defined')
-if (!anon) throw new Error('NEXT_PUBLIC_SUPABASE_ANON_KEY is not defined')
+if (!url) throw new Error("NEXT_PUBLIC_SUPABASE_URL is not defined");
+if (!anon) throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY is not defined");
 
-export const supabase = createClient(url, anon)
+export const supabase = createBrowserClient(url, anon);
