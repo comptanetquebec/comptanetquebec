1c09c5c4321f27ca53d0c3ec286570ad29a5da6c
comptanetquebec <comptanetquebec@gmail.com>
2026-02-10 13:18:27 -0500

    Update layout.tsx

--- app/formulaire-fiscal-presentiel-t2/layout.tsx
+++ app/formulaire-fiscal-presentiel-t2/layout.tsx
@@ -1,5 +1,11 @@
 import "../formulaire-fiscal/formulaire-fiscal-presentiel.css";
+import { requireAdmin } from "@/app/_admin/requireAdmin";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return children;
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  await requireAdmin(); // 🔐 protection admin
+  return <>{children}</>;
 }
