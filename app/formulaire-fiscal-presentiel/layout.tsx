22021d19fa1df3ea8c07bc883642d55e6cf71fd3
comptanetquebec <comptanetquebec@gmail.com>
2026-02-10 13:52:58 -0500

    Create layout.tsx

---
+++ app/formulaire-fiscal-presentiel/layout.tsx
@@ -0,0 +1,6 @@
+// app/formulaire-fiscal-presentiel/layout.tsx
+import "../formulaire-fiscal/formulaire-fiscal-presentiel.css";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return children;
+}
