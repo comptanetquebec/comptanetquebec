7b32a248fe9b0b0a85c49c8482d48ab6b94400cc
comptanetquebec <comptanetquebec@gmail.com>
2026-07-04 18:41:25 -0400

    Create page.tsx

---
+++ app/calculateur-impot-quebec/page.tsx
@@ -0,0 +1,15 @@
+export default function CalculateurImpotPage() {
+  return (
+    <main style={{ maxWidth: 900, margin: "40px auto", padding: "20px" }}>
+      <h1>🧮 Calculateur d'impôt Québec</h1>
+
+      <p>
+        Estimez gratuitement votre remboursement ou votre solde d'impôt.
+      </p>
+
+      <p>
+        Le calculateur sera disponible prochainement.
+      </p>
+    </main>
+  );
+}
