63e472a9137ea97de519e831360a839c2dbc8361
comptanetquebec <comptanetquebec@gmail.com>
2026-09-12 13:01:52 -0400

    Update SpouseSection.tsx

--- app/formulaire-fiscal/sections/SpouseSection.tsx
+++ app/formulaire-fiscal/sections/SpouseSection.tsx
@@ -1344,6 +1344,7 @@ export default function SpouseSection(
             }}
           >
             <YesNoField
+              name="spouse-disability"
               label={
                 <LabelWithMark
                   text={T.disability}
